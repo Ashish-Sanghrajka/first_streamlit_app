2db5ea12640b1d891c1e3aaa5dd682609d18320d
Ashish Sanghrajka <121131137+Ashish-Sanghrajka@users.noreply.github.com>
2022-12-20 13:05:42 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit
+import pandas
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Kale Salad , Beet, cole Slaw')
 streamlit.text('Orange Juice')
 
 streamlit.header('Build Your Own Smoothie')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 
