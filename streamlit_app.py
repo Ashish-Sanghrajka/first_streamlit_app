cb9eddf0c70d309704b36efce511abf6021d3099
Ashish Sanghrajka <121131137+Ashish-Sanghrajka@users.noreply.github.com>
2022-12-22 15:18:42 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,18 @@
 import streamlit
 import pandas
 import snowflake.connector
+import requests
+
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Kale Salad , Beet, cole Slaw')
 streamlit.text('Orange Juice')
-
 streamlit.header('Build Your Own Smoothie')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 streamlit.dataframe(my_fruit_list)
-
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
@@ -26,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.stop()
 
 
-#import requests
+import requests
 #This will Normalize and Flatten the JSON File  
 # This will display the data in a table
 #  streamlit.write('The user entered ', fruit_choice)
