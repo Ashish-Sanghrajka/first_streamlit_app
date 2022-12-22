7542a8991e04a7c561f1067e508f84bd12f25975
Ashish Sanghrajka <121131137+Ashish-Sanghrajka@users.noreply.github.com>
2022-12-22 15:02:34 -0800

    Create my_streamlit2_app.py

---
+++ my_streamlit2_app.py
@@ -0,0 +1,8 @@
+import streamlit
+import pandas
+import snowflake.connector
+
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Kale Salad , Beet, cole Slaw')
+streamlit.text('Orange Juice')
