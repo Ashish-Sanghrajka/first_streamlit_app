f393b07cceac2feb96c6306caf8cb520b1ed331b
Ashish Sanghrajka <121131137+Ashish-Sanghrajka@users.noreply.github.com>
2022-12-22 15:42:09 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ streamlit.dataframe(fruits_to_show)
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
 
      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  fruit_choice)
-     #streamlit.text(fruityvice_response)
      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  fruit_choice)
      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
      streamlit.dataframe(fruityvice_normalized)
