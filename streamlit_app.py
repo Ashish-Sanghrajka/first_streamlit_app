f340b7ce689a79151f4853a4ecbf233657f1e3ce
Ashish Sanghrajka <121131137+Ashish-Sanghrajka@users.noreply.github.com>
2022-12-22 16:22:34 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def get_fruit_load_list():
 # Function Insert row into  Snowflake
 def insert_row_fruit_load_list(new_fruit):
     with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into values ('from streamlit')")
+         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
          return  "Thanks for adding " + new_fruit
 
 streamlit.title('My Parents New Healthy Diner')
